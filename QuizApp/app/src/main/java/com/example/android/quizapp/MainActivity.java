8767face95ecd943a72f48cd4d0e9b7f944894be
rakesh <rakeshcs.2000@gmail.com>
2019-08-18 15:29:56 +0530

    push to git hub

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.quizapp;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    int score = 0;
+
+    /**
+     * Question 1 answer
+     * @param view check RadioButton when the team is selected
+     */
+    public void onRadioButtonClickedQuestion1(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.miami_heat:
+                if (checked)
+                    getIntent().putExtra("score", 1);
+                break;
+            case R.id.new_york_knicks:
+                if (checked)
+                    break;
+            case R.id.chicago_bulls:
+                if (checked)
+                    break;
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
